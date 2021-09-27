80e906257e0a61858ef813ca63d66d0000b843cd
motoki418 <snkrs418@gmail.com>
2021-09-27 11:17:28 +0900

    #1 HealthKitDataGet

--- walking/View/ReportView.swift
+++ walking/View/ReportView.swift
@@ -9,36 +9,16 @@ import SwiftUI
 import HealthKit
 
 struct ReportView: View {
-    private var healthStore = HealthStore()
-    
-    @State private var steps: [Step] = []
-    
-    private func updateUIFromStatistics(_ statisticsCollection: HKStatisticsCollection){
-        
-        let startDate = Calendar.current.date(byAdding: .day, value: -6, to: Date())!
-        let endDate = Date()
-        
-        statisticsCollection.enumerateStatistics(from: startDate, to: endDate){ (statistics, stop) in
-            
-            let count = statistics.sumQuantity()?.doubleValue(for: .count())
-            
-            let step = Step(count: Int(count ?? 0), date: statistics.startDate)
-            steps.append(step)
-        }
-    }
-    //Pickerで表示するテキストを配列で管理
-    let date = ["週間","月間","年間"]
+    //Pickerで表示するテキストを配列で保持する変数
+    private let date = ["週間","月間","年間"]
     //Pickerの値が変更できるように状態変数で宣言
     @State private var selection = 0
     
     var body: some View {
         ZStack(alignment: .top){
             NavigationView{
-                List(steps, id: \.id) { step in
-                    VStack{
-                        Text("\(step.count)歩")
-                        Text(step.date, style: .date)
-                    }
+                VStack{
+                    Text("Hello World")
                 }
                 .navigationBarTitleDisplayMode(.inline)
             }//NavigationView
@@ -50,21 +30,6 @@ struct ReportView: View {
             .pickerStyle(SegmentedPickerStyle())
             .frame(width:200)
         }//ZStack
-        .onAppear{
-            //データの読み取りの権限をリクエスト
-            healthStore.requestAuthorization{ success in
-                //もしデータの読み取りの権限をリクエストしてアクセスできたら
-                if success{
-                    print("データにアクセスできます！")
-                    healthStore.calculateSteps{ statisticsCollection in
-                        if let statisticsCollection = statisticsCollection{
-                            print(statisticsCollection)
-                            updateUIFromStatistics(statisticsCollection)
-                        }
-                    }
-                }// if success
-            }
-        }//.onAppear
     }
 }
 
